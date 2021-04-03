0fdc09f2ecbdbc0762a43aa1f194e3fb29ca7e57
moaaz-bhnas <moaaz_bs@yahoo.com>
2021-04-03 02:59:00 +0200

    language button

--- src/components/header/LogoLink.js
+++ src/components/header/LogoLink.js
@@ -1,16 +1,20 @@
 import { memo } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import styled from "styled-components";
 import Logo from "../logo/Logo";
+import getDirection from "../../utils/helpers/getDirection";
 
 const StyledLink = styled.a`
   margin-right: 1em;
 `;
 
 const LogoLink = () => {
+  const { locale } = useRouter();
+
   return (
     <Link href="/" passHref>
-      <StyledLink>
+      <StyledLink dir={getDirection(locale)}>
         <Logo />
       </StyledLink>
     </Link>
